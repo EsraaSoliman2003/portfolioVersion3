1b132282d39ea1c28ce92d0b01a1d060626a99d8
Esraa Soliman <ee341110@gmail.com>
2025-08-13 00:01:10 +0300

    fix db

--- api/visits.js
+++ api/visits.js
@@ -1,7 +1,17 @@
-// api/visits.js
-let visits = 0;
+import { Redis } from "@upstash/redis";
 
-export default function handler(req, res) {
-  visits++;
-  res.status(200).json({ visits });
+const redis = new Redis({
+  url: import.meta.env.VITE_UPSTASH_URL,
+  token: import.meta.env.VITE_UPSTASH_TOKEN,
+});
+
+export async function handler(req, res) {
+  try {
+    // نزود العداد (مفتاح visits)
+    const visits = await redis.incr("visits");
+    res.status(200).json({ visits });
+  } catch (error) {
+    console.error("Redis error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
